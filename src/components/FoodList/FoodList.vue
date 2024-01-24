56010c77dc7f80d35dffbc6275d5d14c4590a8b6
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-24 10:55:35 +0300

    feat: add card of list-cards

---
+++ src/components/FoodList/FoodList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onUpdated } from "vue";
+import FoodListCard from "../FoodListCard/FoodListCard.vue";
+
+
+
+</script>
+
+<template>
+    <div class="food-list">
+        <div class="food-list__title"></div>
+        <div class="food-list__cards">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FoodListCard/FoodListCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="food-list-card">
+        <div class="food-list-card__img">
+            <img src="" alt="">
+        </div>
+        <div class="food-list-card__description">
+            <div class="food-list-card__description-wrapper">
+                <div class="food-list-card__title"></div>
+                <div class="food-list-card__characteristic"></div>
+            </div>
+            <div class="food-list-card__cost-button">
+                <div class="food-list-card__cost"></div>
+                <button class="food-list-card__button"></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
