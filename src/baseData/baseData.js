d586c32b3fd19145f694b68162248e99a195e4b8
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-24 10:54:58 +0300

    feat: add base-data

---
+++ src/baseData/baseData.js
@@ -0,0 +1,13 @@
+export const products = {
+    pizza: [
+        {
+            name: 'Двойной цыпленок',
+            sizes: {
+                small: {diameter: '25', weight: '360', image: "../assets/pizza-image/chicken-small.png"},
+                middle: {diameter: '30', weight: '520'},
+                big: {diameter: '35', weight: '730'},
+            },
+            composition: "Цыпленок, моцарелла, фирменный соус альфредо",
+        }
+    ],
+}
