b25fe26a190e727edb77483fc8354c1f3795969c
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-24 13:16:26 +0300

    feat(HomePage): add main page

---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Banner from "../Banner/Banner.vue";
+import FoodList from "../FoodList/FoodList.vue";
+</script>
+
+<template>
+    <Banner />
+    <FoodList />
+</template>
+
+<style scoped>
+
+</style>
