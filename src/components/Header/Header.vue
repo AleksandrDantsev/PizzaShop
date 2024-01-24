aeb547299ddc2a95f02015ece11ce89664601b42
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-24 13:18:46 +0300

    feat: add function for get link

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,37 +1,57 @@
 <script setup>
+import { provide, ref } from 'vue';
+const currentLink = ref();
 
+function checkLink(e) {
+    if (e.target.tagName === "A") {
+        currentLink.value = getLastLink(e.target.href);
+        
+    }
+}
+
+function getLastLink(fullLink) {
+     return fullLink.split('/').at(-1);
+}
+console.log(currentLink)
+provide('link', 'hello!')
 </script>
 
 <template>
   <header class="header">
     <div class="header__wrapper">
-        <ul class="header__list">
-            <li><router-link to="/">Пиццы</router-link></li>
-            <li><router-link to="/kombo">Комбо</router-link></li>
-            <li><router-link to="/snack">Закуски</router-link></li>
-            <li><router-link to="/cocktail">Коктейли</router-link></li>
-            <li><router-link to="/coffee">Кофе</router-link></li>
-            <li><router-link to="/drink">Напитки</router-link></li>
+        <ul class="header__list" @click.prevent="checkLink" >
+            <li><a href="/">Пиццы</a></li>
+            <li><a href="/kombo">Комбо</a></li>
+            <li><a href="/snack">Закуски</a></li>
+            <li><a href="/cocktail">Коктейли</a></li>
+            <li><a href="/coffee">Кофе</a></li>
+            <li><a href="/drink">Напитки</a></li>
         </ul>
         <button class="header__basket">Корзина</button>
     </div>
+    
   </header>
 </template>
 
 <style scoped lang="scss">
 .header {
+    position: relative;
     display: flex;
     align-items: center;
     width: 100%;
-    height: 70px;
+    height: 80px;
     box-shadow: 0 1px 15px 0 #df8d5750;
+    background-color: #fff;
 }
 .header__wrapper {
+    
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: 80%;
+    height: 100%;
     margin: 0 auto;
+
 }
 .header__list {
     display: flex;
@@ -54,4 +74,5 @@
         box-shadow: 0 1px 12px 0 #df8d57ad;
     }
 }
+
 </style>
