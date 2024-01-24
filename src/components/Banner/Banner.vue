ccf1ada2bd9e8376e5f0c9f52006e9202f4da28b
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-24 13:15:42 +0300

    chore: redirect banner

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="header__banner">
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .header__banner {
+    position: absolute;
+    z-index: -1;
+    left: 0;
+    right: 0;
+    top: 0;
+    height: 100vh;
+    background: url('@/assets/banner.jpg') 0 0 / 100% 100% no-repeat;
+}
+</style>
