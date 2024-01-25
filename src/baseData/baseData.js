ed94378809004302dcee29ac75e32724a3c006ba
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-25 19:34:06 +0300

    chore: add funds base-data

--- src/baseData/baseData.js
+++ src/baseData/baseData.js
@@ -2,12 +2,70 @@ export const products = {
     pizza: [
         {
             name: 'Двойной цыпленок',
+            image: "/public/assets/pizza-image/chicken-middle.png",
             sizes: {
-                small: {diameter: '25', weight: '360', image: "../assets/pizza-image/chicken-small.png"},
-                middle: {diameter: '30', weight: '520'},
-                big: {diameter: '35', weight: '730'},
+                small: {diameter: '25', weight: '360', image: "/public/assets/pizza-image/chicken-small.png"},
+                middle: {diameter: '30', weight: '520', image: "/public/assets/pizza-image/chicken-middle.png"},
+                big: {diameter: '35', weight: '730', image: "/public/assets/pizza-image/chicken-big.png"},
             },
+            cost: '450',
             composition: "Цыпленок, моцарелла, фирменный соус альфредо",
+        },
+        {
+            name: 'Пепперони фреш',
+            image: "/public/assets/pizza-image/chicken-middle.png",
+            sizes: {
+                small: {diameter: '25', weight: '380', image: "/public/assets/pizza-image/chicken-small.png"},
+                middle: {diameter: '30', weight: '590', image: "/public/assets/pizza-image/chicken-middle.png"},
+                big: {diameter: '35', weight: '790', image: "/public/assets/pizza-image/chicken-big.png"},
+            },
+            cost: '450',
+            composition: "Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус",
+            
+        },
+        {
+            name: 'Пепперони фреш',
+            image: "/public/assets/pizza-image/chicken-middle.png",
+            sizes: {
+                small: {diameter: '25', weight: '380', image: "/public/assets/pizza-image/chicken-small.png"},
+                middle: {diameter: '30', weight: '590', image: "/public/assets/pizza-image/chicken-middle.png"},
+                big: {diameter: '35', weight: '790', image: "/public/assets/pizza-image/chicken-big.png"},
+            },
+            cost: '450',
+            composition: "Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус",
+            
+        },
+        {
+            name: 'Пепперони фреш',
+            image: "/public/assets/pizza-image/chicken-middle.png",
+            sizes: {
+                small: {diameter: '25', weight: '380', image: "/public/assets/pizza-image/chicken-small.png"},
+                middle: {diameter: '30', weight: '590', image: "/public/assets/pizza-image/chicken-middle.png"},
+                big: {diameter: '35', weight: '790', image: "/public/assets/pizza-image/chicken-big.png"},
+            },
+            cost: '450',
+            composition: "Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус",
+            
+        }
+    ],
+    coffee: [
+        {
+            name: "Кофе Карамельный капучино", 
+            size: '0,3', 
+            weight: "230",
+            image: "https://dodopizza-a.akamaihd.net/static/Img/Products/5077dd1995e544539c79e5f639b244bb_584x584.webp", 
+            composition: `Если не шоколад, то карамель! А капучино с карамельным сиропом особенно хорош`,
+            cost: '450',
+        }
+        
+    ],
+    snack: [
+        {
+            name: "Додстер", 
+            weight: "210",
+            image: "https://dodopizza-a.akamaihd.net/static/Img/Products/be03844e8b8a401ba4af5a4e7f74ea37_1875x1875.webp", 
+            composition: `Легендарная горячая закуска с цыпленком, томатами, моцареллой, соусом ранч в тонкой пшеничной лепешке`,
+            cost: '200',
         }
     ],
 }
