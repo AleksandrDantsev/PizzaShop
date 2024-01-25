416d962386ab150f5a2c938a50ac514097453d3f
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-25 21:54:49 +0300

    chore: add additive to order data

---
+++ src/baseData/additives.js
@@ -0,0 +1,11 @@
+export const additives = [
+    {name: 'Моцарелла', cost: 70, image: "/assets/additives/mozzarella.png", id: 'mozzarella'},
+    {name: 'Ветчина', cost: 85, image: "/assets/additives/ham.png", id: 'ham'},
+    {name: 'Помидор', cost: 70, image: "/assets/additives/tomat.png", id: 'tomat'},
+    {name: 'Ялтинский лук', cost: 50, image: "/assets/additives/onion.png", id: 'onion'},
+    {name: 'Сладкий перец', cost: 65, image: "/assets/additives/pepper.png", id: 'pepper'},
+    {name: 'Маринованный огурец', cost: 93, image: "/assets/additives/marinated_cucumber.png", id: 'marinated_cucumber'},
+    {name: 'Шампиньоны', cost: 80, image: "/assets/additives/mushrooms.png", id: 'mushrooms'},
+    {name: 'Халапеньо', cost: 65, image: "/assets/additives/bitter_pepper.png", id: 'bitter_pepper'},
+    {name: 'Пармезан', cost: 95, image: "/assets/additives/parmesan.png", id: 'parmesan'},
+];
